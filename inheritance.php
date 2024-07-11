de9661f879894a776b8bf68784a3cb05720299cd
RKP <ajarubab@gmail.com>
2024-07-11 19:18:12 +0530

    Inheritance Child class same name local variable value unique  Scenerio

--- inheritance.php
+++ inheritance.php
@@ -1,21 +1,26 @@
 <?php
     class Class1 {
+        public $x;
         function __construct() {
-            echo " Class1 Contructor";
+            $this->x = 5 ;
+            // echo " Class1 Contructor<br>";
         }
 
         function fun1() {
-            echo "Class1 function.";
+            // echo "Class1 function.";
         }
     }
     
     class Class2 extends Class1{
         function __construct() {
-            echo " Class2 Contructor";
+            // parent::__construct();      // results 90                       // calls parent class contructor
+            $this->x = 90 ;
+            parent::__construct();          // results 5
+            // echo "Class2 Contructor";
         }
 
         function fun1() {
-            echo "Class2 function.";
+            // echo "Class2 function.";
         }
     }
 
@@ -28,5 +33,15 @@
      */
 
     echo "<br>";
+    echo "<br>";
     $obj->fun1();
+
+    /**
+     * if we put the parent class contructor calling statement before
+     * the same child class variable name declaration statement
+     * we will get the output of variable x of child class value i.e 90 here
+     * but if we put our child class local vaiable name declaration above the
+     * parant class contructor calling statement, we will get  the value of parent class variable i.e 5 here
+     */
+    echo $obj->x;
 ?>

d6b5de029606dd45b3baf81ff4ba1a1f9352cc92
RKP <ajarubab@gmail.com>
2024-07-11 18:53:40 +0530

    Inheritance Child class contructor calling Scenerio

--- inheritance.php
+++ inheritance.php
@@ -6,7 +6,14 @@
     }
     
     class Class2 extends Class1{
+        function __construct() {
+            echo " Class2 Contructor";
+        }
     }
 
     $obj = new Class2();
+    /**
+     * if the child class has it's own contructor 
+     * then on making object only the child class constructor will execute.
+     */
 ?>
